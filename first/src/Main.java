629f7b776b3c5d773f23f0c0e5d4e823fced5dd0
prisakura <leon.prisacaru@gmail.com>
2024-07-07 08:10:31 +0300

    Array reverse

--- first/src/Main.java
+++ first/src/Main.java
@@ -1,5 +1,27 @@
+import java.sql.Array;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(1);
+        int[] array = new int[11];
+        //random array
+        for (int itter = 0; itter < array.length; itter++) {
+
+            array[itter] = new Random().nextInt(100);
+            System.out.print(array[itter]+"\t");
+        }
+        System.out.println();
+        //reverse
+        for (int itter = 0; itter < array.length/2;itter++) {
+            int tmp = array[itter];
+            array[itter]=array[array.length-itter-1];
+            array[array.length-itter-1]=tmp;
+        }
+        //print
+        for (int itter = 0; itter < array.length; itter++) {
+            System.out.print(array[itter]+"\t");
+        }
     }
 }
+
